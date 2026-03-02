26d819a6a1ed0611738097c62740b6012c3b0f3c
Ruben Jimenez Andreu <rubenjmnz93@gmail.com>
2026-03-02 21:06:55 +0100

    Push nginx image with cp2 tag to acr

--- ansible/inventory.py
+++ ansible/inventory.py
@@ -1,42 +1,55 @@
 #!/usr/bin/env python3
 import json
 import subprocess
+import sys
 
-tf = json.loads(subprocess.check_output(
-    ["terraform", "output", "-json"], cwd="../terraform"
-))
 
-ip = tf["vm_ip"]["value"]
-user = tf["vm_username"]["value"]
-private_key = tf["vm_private_key"]["value"]
-acr_name = tf["acr_name"]["value"]
+def get_inventory():
+    tf = json.loads(subprocess.check_output(
+        ["terraform", "output", "-json"], cwd="../terraform"
+    ))
 
-key_path = "../ssh/id_rsa"
-with open(key_path, "w") as f:
-    f.write(private_key)
+    ip = tf["vm_ip"]["value"]
+    user = tf["vm_username"]["value"]
+    private_key = tf["vm_private_key"]["value"]
+    acr_name = tf["acr_name"]["value"]
+    acr_password = tf["acr_password"]["value"]
 
-inventory = {
-    "vm": {
-        "hosts": [ip],
-        "vars": {
-            "ansible_user": user,
-            "ansible_ssh_private_key_file": key_path
-        }
-    },
-    "acr" : {
-        "hosts": ["localhost"]
-    },
-    "all": {
-        "vars": {
-            "acr_name": acr_name
-        }
-    },
-    "localhost": {
-        "hosts": ["127.0.0.1"],
-        "vars": {
-            "ansible_connection": "local"
+    key_path = "../ssh/id_rsa"
+    with open(key_path, "w") as f:
+        f.write(private_key)
+
+    inventory = {
+        "vm": {
+            "hosts": [ip],
+            "vars": {
+                "ansible_user": user,
+                "ansible_ssh_private_key_file": key_path
+            }
+        },
+        "acr" : {
+            "hosts": ["localhost"],
+            "vars": {
+                "ansible_connection": "local",
+                "acr_password": acr_password
+            }
+        },
+        "all": {
+            "vars": {
+                "acr_name": acr_name
+            }
+        },
+        "_meta": {
+            "hostvars": {}
         }
     }
-}
 
-print(json.dumps(inventory))
+    return inventory
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1] == "--list":
+        print(json.dumps(get_inventory()))
+    elif len(sys.argv) == 3 and sys.argv[1] == "--host":
+        print(json.dumps({}))
+    else:
+        print(json.dumps(get_inventory()))
--- terraform/output.tf
+++ terraform/output.tf
@@ -7,7 +7,12 @@ output "vm_username" {
 }
 
 output "acr_name" {
-  value = azurerm_container_registry.acr.admin_username
+  value = azurerm_container_registry.acr.name
+}
+
+output "acr_password" {
+  value = azurerm_container_registry.acr.admin_password
+  sensitive = true
 }
 
 output "vm_private_key" {
